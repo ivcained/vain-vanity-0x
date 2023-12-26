a85fdd2603b0a48fceffe9a7a0900ab2b3ec2a6a
ivcained <86070833+ivcained@users.noreply.github.com>
2023-12-26 16:16:18 +0530

    chore: net work

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -3,15 +3,15 @@
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { Mumbai, Optimism } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+export const NETWORK = Optimism;
 
 // 2. The address of the Vain smart contract.
 // Contracts here:
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
+export const MARKETPLACE_ADDRESS = "0x000000000001F04A9533e92d7AD4dDe7DC19a8F3";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+  "0x000000000001F04A9533e92d7AD4dDe7DC19a8F3";
 
 // URL of where users can view transactions on
 // Mumbai testnet and OP Mainnet
