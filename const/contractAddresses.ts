af21944ed3b16e63a6f8f9ed2872c076fbc291fb
ivcained <86070833+ivcained@users.noreply.github.com>
2024-01-07 00:37:25 +0530

    chore: networks

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,14 +7,14 @@ export const NETWORK = Optimism;
 
 // 2. The address of the Vain smart contract.
 // Contracts here:
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
+export const MARKETPLACE_ADDRESS = "0x000000000001F04A9533e92d7AD4dDe7DC19a8F3";
 
 // 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+  "0x000000000001F04A9533e92d7AD4dDe7DC19a8F3";
 
 // URL of where users can view transactions on
 // Mumbai testnet and OP Mainnet
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+//export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
 // Optimism Scan.
-//export const ETHERSCAN_URL = "https://optimistic.etherscan.io/";
+export const ETHERSCAN_URL = "https://optimistic.etherscan.io/";
